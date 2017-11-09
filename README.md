91157fb541d5f7040b62bd0a91e0e4561a081189
Sergiy Beley <Sergiy.Beley@gmail.com>
2017-11-09 22:32:00 +0200

    Fixed compile errors

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
-[![Build Status](https://travis-ci.org/ghale/gradle-jenkins-plugin.svg?branch=master)](https://travis-ci.org/crc83/gradle-jenkins-plugin.svg?branch=master)
+[![Build Status] (https://travis-ci.org/ghale/gradle-jenkins-plugin.svg?branch=master)](https://travis-ci.org/crc83/gradle-jenkins-plugin.svg?branch=master)
+
+[![Coverage Status](https://coveralls.io/repos/github/crc83/gradle-jenkins-plugin/badge.svg?branch=master)](https://coveralls.io/github/crc83/gradle-jenkins-plugin?branch=master)
+
 gradle-jenkins-plugin
 =====================
+
 Forked from here : https://github.com/ghale/gradle-jenkins-plugin
+
 Gradle version updated to 4.3.1
+
 I plan to update versions of gradle and dependencies
+
 Gradle plugin to programmatically configure Jenkins jobs.  This plugin allows you to maintain jenkins job configurations in source control and apply them to the server via gradle.  Jobs can be stored as straight xml files, xml strings, markup builder closures, or jenkins job dsl.  Job templates can be defined that can then be manipulated such that multiple jobs can be generated off of a single template definition.
 
 See https://github.com/ghale/gradle-jenkins-plugin/wiki for details on usage.
--- src/main/groovy/com/terrafolio/gradle/plugins/jenkins/jobdsl/MapJobManagement.groovy
+++ src/main/groovy/com/terrafolio/gradle/plugins/jenkins/jobdsl/MapJobManagement.groovy
@@ -2,8 +2,6 @@ package com.terrafolio.gradle.plugins.jenkins.jobdsl
 
 import com.google.common.collect.Maps
 import javaposse.jobdsl.dsl.AbstractJobManagement
-import javaposse.jobdsl.dsl.ConfigFile
-import javaposse.jobdsl.dsl.ConfigFileType
 import javaposse.jobdsl.dsl.ConfigurationMissingException
 import javaposse.jobdsl.dsl.Item
 import javaposse.jobdsl.dsl.JobConfigurationNotFoundException
@@ -37,11 +35,6 @@ class MapJobManagement extends AbstractJobManagement {
     }
 
     @Override
-    String createOrUpdateConfigFile(ConfigFile configFile, boolean ignoreExisting){
-        return null
-    }
-
-    @Override
     void createOrUpdateView(String viewName, String config, boolean ignoreExisting) throws NameNotProvidedException, ConfigurationMissingException {
         validateUpdateArgs(viewName, config)
         map.put(viewName, config)
@@ -61,6 +54,13 @@ class MapJobManagement extends AbstractJobManagement {
     }
 
     @Override
+    boolean isMinimumCoreVersion(String version) {
+        VersionNumber minimumVersion = new VersionNumber(version)
+        System.out.println("CORE VERSION:"+minimumVersion);
+        return true;//temporary
+    }
+
+    @Override
     void requireMinimumPluginVersion(String pluginShortName, String version){
     }
 
@@ -70,11 +70,6 @@ class MapJobManagement extends AbstractJobManagement {
 
     @Override
     Integer getVSphereCloudHash(String name){
-        return null
-    }
-
-    @Override
-    String getConfigFileId(ConfigFileType type, String name){
         return null
     }
 
